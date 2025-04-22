15b19ba42b59c08ab909b5a2fac830404c31133a
svaldez831 <samuel.valdez@bsu.edu>
2025-04-22 06:39:04 +0000

    created Mode Selection = simple admin and simple task mode

--- HouseholdTaskManager/TaskManager/Program.cs
+++ HouseholdTaskManager/TaskManager/Program.cs
@@ -2,8 +2,37 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    static void Main(string[] args) { 
+        bool runProgram = true;
+        string choice;
+        Console.Write("Please select Mode (Admin or User)");
+        string mode = Console.ReadLine();
+
+        if(mode == "User") {
+            do {
+                Console.Write("SELECT AN OPTION OR 'END' TO EXIT");
+                choice = Console.ReadLine();
+                if(choice == "END") {
+                    runProgram = false;
+                }
+                else {
+
+                }
+                
+            } while(runProgram);
+        } else {
+            do {
+                Console.Write("Hello ADMIN Select AN OPTION OR 'END' TO EXIT");
+                choice = Console.ReadLine();
+                if(choice == "END") {
+                    runProgram = false;
+                }
+                else {
+
+                }
+
+            } while(runProgram);
+        }
     }
+        
 }
