f21b3f44cb14153d34236662e99b78f292e3b4e0
svaldez831 <samuel.valdez@bsu.edu>
2025-05-01 06:09:17 +0000

    cleaning up, removing old functions.

--- HouseholdTaskManager/TaskManager/FileHelper.cs
+++ HouseholdTaskManager/TaskManager/FileHelper.cs
@@ -46,55 +46,6 @@ public static class FileHelper {
         return tasks;
     }
 
-    static void SaveBillToFile(List<Bill> bills) {
-        string textFile = "records.txt";
-        using (StreamWriter sw = new StreamWriter(textFile, append: true)) {
-            foreach(var bill in bills) {
-                sw.WriteLine($"{bill.Id}, {bill.Type},{bill.Assignee},{bill.Name},{bill.DateCreated},{bill.DateModified},{bill.DueDate},{bill.Paid},{bill.Instructions},{bill.Notes}");
-            }
-        }
-    }
-
-    public static Task ParseTask(List<string> cols) {
-
-        if (cols.Count < 10 || cols[1] != "Task") 
-            return null;
-
-        var tempTask = new Task(cols[2], cols[3], cols[6], cols[8]);
-        tempTask.Id = cols[0];
-        tempTask.DateCreated = cols[4];
-        tempTask.DateModified = cols[5];
-        tempTask.DueDate = cols[6];
-        tempTask.Completed = bool.TryParse(cols[7], out var completed) && completed;
-        tempTask.Instructions = cols[8];
-        tempTask.Notes = cols[9];
-
-        return tempTask;
-    }
-
-    public static List<Task> ParseTasks(List<List<string>> rows) {
-        var tempTasks = new List<Task>();
-        foreach (var row in rows) {
-            var task = ParseTask(row);
-            if(task != null) {
-                tempTasks.Add(task);
-            }
-        }
-        return tempTasks;
-    }
-
-
-
-
-    public static List<List<string>> ReadCsvTo2Dlist(string textFile ){
-        var result = new List<List<string>>();
-    
-        foreach(var line in File.ReadLines(textFile)) {
-            var values = line.Split(',');
-            result.Add(new List<string>(values));
-        }
-        return result;
-    }
 
      public static List<string> GenerateUsers(string textFile) {
         List<string> lines = File.ReadLines("records.txt").ToList();
@@ -106,24 +57,6 @@ public static class FileHelper {
                 users.Add(columns[2]);
             }
         }
-        return users;
-    }
-
-    public static List<string> GenerateUsers2(List<Task> tasks, List<Bill> bills) {
-        List<string> users = new List<string>();
-
-        foreach (Task task in tasks) {
-            if(!users.Contains(task.Assignee)) {
-                users.Add(task.Assignee);
-            }
-        }
-
-        foreach (Bill bill in bills) {
-            if(!users.Contains(bill.Assignee)) {
-                users.Add(bill.Assignee);
-            }
-        }
-
         return users;
     }
 
@@ -176,41 +109,6 @@ public static class FileHelper {
 
         File.WriteAllLines(filePath, rows);
     }
-
-
-
-    public  static void SaveToFile(List<string> data ) {
-        List<string> lines = File.ReadLines("records.txt").ToList();
-    
-        for(int i = 0; i < lines.Count; i++) {
-            string[] columns = lines[i].Split(',');
-
-            if(columns[0].Trim() == data[0].Trim()) {
-                columns[1] = data[1];
-                columns[2] = data[2];
-                columns[3] = data[3];
-                columns[4] = data[4];
-                columns[5] = data[5];
-                columns[6] = data[6];
-                columns[7] = data[7];
-                columns[8] = data[8];
-                columns[9] = data[9];
-
-                lines[i] = string.Join(",", columns);
-                break;
-            }
-    
-        }
-    
-
-    
-        File.WriteAllLines("records.txt",lines);
-
-    }
-
-
-
-
 
 
 }
--- HouseholdTaskManager/TaskManager/RecordFilter.cs
+++ HouseholdTaskManager/TaskManager/RecordFilter.cs
@@ -2,60 +2,6 @@ namespace TaskManager;
 using System;
 using System.IO;
 public static class RecordFilter {
-
-    
-
-    public static List<List<string>> ExtractTasks (List<List<string>> data) {
-        var result = new List<List<string>>();
-        string keyword = "Task";
-        foreach (var row in data) {
-            if (row.Count > 1 && row[1] == keyword) {
-                result.Add(new List<string>(row));
-            }
-        }
-
-        
-        return result;
-    } 
-
-    public static List<Bill> ExtractBills (List<List<string>> data) {
-        var result = new List<Bill>();
-        string keyword = "Bill";
-        foreach (var row in data) {
-            if (row.Count > 1 && row[1] == keyword) {
-                var bill = new Bill(row[2], row[3], row[6],row[8]);
-                bill.Id = row[0];
-                bill.DateCreated = row[4];
-                bill.DateModified = row[5];
-                bill.DueDate = row[6];
-                bill.Paid = bool.TryParse(row[7], out var paid) && paid;
-                bill.Instructions = row[8];
-                bill.Notes = row[9];
-                result.Add(bill);
-            }
-        }
-
-        
-        return result;
-
-    }
-
-    public static List<List<string>> ExtractByUser (List<List<string>> data, string user) {
-        var result = new List<List<string>>();
-        string keyword = user;
-        
-        foreach (var row in data) {
-            if (row.Count > 1 && row[2] == keyword) {
-                result.Add(new List<string>(row));
-                
-            }
-        }
-
-        
-        return result;
-
-    } 
-
     public static List<Task> ExtractTaskByUser(List<Task> tasks, string user) {
         List<Task> result = new List<Task>();
         foreach(Task task in tasks) {
