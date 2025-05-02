a43b59fcd973a0f1a8f76053e36bfa996936952d
svaldez831 <samuel.valdez@bsu.edu>
2025-05-02 08:16:25 +0000

    added another test to record filter test

--- HouseholdTaskManager/HouseHoldTaskManager.Tests/RecordFilterTest.cs
+++ HouseholdTaskManager/HouseHoldTaskManager.Tests/RecordFilterTest.cs
@@ -27,6 +27,23 @@ public class RecordFilterTests {
         Assert.Equal("Bill1", user[0].Name);
     }
 
+    [Fact]
+    public void ExtractTasksCompletedReturnCompletedOnly() {
+        var tasks = new List<Task> {
+            new Task ("Alice", "Task1", "05-6-2025", "task1"),
+            new Task ("Bob", "Task2", "05-6-2025", "task2"),
+            new Task ("Sam", "Task1", "05-6-2025", "task3"),
+            new Task ("Chuy", "Task2", "05-6-2025", "task4"),
+        };
+        tasks[0].Completed = true;
+        tasks[3].Completed = true;
+       var extracted =  RecordFilter.ExtractTasksCompleted(tasks);
+       Assert.Equal(2, extracted.Count);
+       Assert.True( extracted[0].Completed);
+       Assert.True( extracted[1].Completed);
+     
+    }
+    
 
 
 
